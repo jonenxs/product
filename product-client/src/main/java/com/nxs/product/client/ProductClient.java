9e02e5398040a4e81e8e86bb290ac29350f87c8d
jonenxs <570141370@qq.com>
2018-04-23 00:10:38 +0800

    修改版本

--- product-client/src/main/java/com/nxs/product/client/ProductClient.java
+++ product-client/src/main/java/com/nxs/product/client/ProductClient.java
@@ -2,13 +2,13 @@ package com.nxs.product.client;
 
 import com.nxs.product.common.DecreaseStockInput;
 import com.nxs.product.common.ProductInfoOutput;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "product")
+@FeignClient("product")
 public interface ProductClient {
     @PostMapping("/product/listForOrder")
     List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
