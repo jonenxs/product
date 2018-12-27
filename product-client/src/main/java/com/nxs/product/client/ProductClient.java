81ffa20c49eb76c1d0637e9385721127bba628a9
jonenxs <570141370@qq.com>
2018-12-27 21:30:20 +0800

    商品和订单服务结合使用MQ

--- product-client/src/main/java/com/nxs/product/client/ProductClient.java
+++ product-client/src/main/java/com/nxs/product/client/ProductClient.java
@@ -3,16 +3,32 @@ package com.nxs.product.client;
 import com.nxs.product.common.DecreaseStockInput;
 import com.nxs.product.common.ProductInfoOutput;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient("product")
+@FeignClient(value = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
+
     @PostMapping("/product/listForOrder")
     List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient {
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
+
 }
--- product-server/src/main/java/product/controller/ProductController.java
+++ product-server/src/main/java/product/controller/ProductController.java
@@ -69,7 +69,9 @@ public class ProductController {
      * @return
      */
     @PostMapping("/listForOrder")
-    public List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList){
+    public List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList) throws InterruptedException {
+
+//        Thread.sleep(2000L);
         return productService.findList(productIdList);
     }
 
